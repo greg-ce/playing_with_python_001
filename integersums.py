2635b99ed448d7d49a52d26b1f11d7841f6e6c51
Grzegorz Celej <g.celej@gmail.com>
2020-10-12 18:28:18 +0200

    Commit 1

--- integersums.py
+++ integersums.py
@@ -5,3 +5,10 @@
 
     The function should return 0 if a non-integer is passed in.
 """
+def first(n):
+    num = 1
+    sum = 0
+    while num < n + 1:
+        sum = sum + num
+        num = num + 1
+    return sum
