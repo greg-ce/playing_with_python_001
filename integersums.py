e1593fd7f30149522dae4e43b82b05b808d3f7a4
Grzegorz Celej <g.celej@gmail.com>
2020-10-12 18:36:50 +0200

    Commit 2

--- integersums.py
+++ integersums.py
@@ -12,3 +12,19 @@ def first(n):
         sum = sum + num
         num = num + 1
     return sum
+
+def better(n):
+    sum = 0
+    for num in range(n + 1):
+        sum += num
+    return sum
+
+def even_better(n):
+    return sum(range(n + 1))
+
+def add_it_up(n):
+    try:
+        result = sum(range(n + 1))
+    except TypeError:
+        result = 0
+    return result
