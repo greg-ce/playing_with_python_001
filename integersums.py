6f9e3204474553c6052c303573d44c73dcaded15
Grzegorz Celej <g.celej@gmail.com>
2020-10-14 12:12:23 +0200

    Added logparse.py

--- integersums.py
+++ integersums.py
@@ -1,10 +1,12 @@
-# integersums.py
+#https://realpython.com/python-practice-problems/
+
 """ Sum of Integers Up To n
     Write a function, add_it_up(), that takes a single integer as input
     and returns the sum of the integers from zero to the input parameter.
 
     The function should return 0 if a non-integer is passed in.
 """
+
 def first(n):
     num = 1
     sum = 0
